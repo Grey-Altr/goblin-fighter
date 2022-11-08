91fc109b78009d18f5d4a23f8dc070634dd8b69e
Grey Altr <grey.sifr@gmail.com>
2022-11-08 11:15:27 -0800

    added form eventListener, and states for spawning and inviting robits

--- app.js
+++ app.js
@@ -3,12 +3,34 @@
 /* Get DOM Elements */
 const playerFPEl = document.querySelector('#player-fp');
 const befriendedNumEl = document.querySelector('#befriend-num');
+const form = document.querySelector('form');
 /* State */
-let playerFP = 100;
+let playerFP = 10;
 let befriendedRobitCount = 0;
+let currentId = 3;
+let robits = [
+    { id: 1, name: '451-1', fp: 3 },
+    { id: 2, name: '83nn1', fp: 4 },
+    { id: 3, name: '573v3', fp: 7 },
+];
 /* Events */
 playerFPEl.textContent = playerFP;
 befriendedNumEl.textContent = befriendedRobitCount;
+
+// Invite Robit
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    const robitName = data.get('goblin-name');
+    const newRobit = {
+        id: currentId,
+        name: robitName,
+        fp: Math.ceil(Math.random() * 5),
+    };
+
+    currentId++;
+    robits.push(newRobit);
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
