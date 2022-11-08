48299da66c07ab37a0424324722f41fa822a068a
Grey Altr <grey.sifr@gmail.com>
2022-11-08 10:26:19 -0800

    added befriended attribute to player

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 /* Imports */
 
 /* Get DOM Elements */
-const playerFPEl = document.querySelector('player-fp');
+const playerFPEl = document.querySelector('#player-fp');
+const befriendedNumEl = document.querySelector('#befriend-num');
 /* State */
 let playerFP = 100;
+let befriendedRobitCount = 0;
 /* Events */
 playerFPEl.textContent = playerFP;
+befriendedNumEl.textContent = befriendedRobitCount;
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
