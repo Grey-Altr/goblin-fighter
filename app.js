d90c091787a5b49927484e27d39cf6045ccb59d6
Grey Altr <grey.sifr@gmail.com>
2022-11-08 10:14:37 -0800

    added player attributes to app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const playerFPEl = document.querySelector('player-fp');
 /* State */
-
+let playerFP = 100;
 /* Events */
-
+playerFPEl.textContent = playerFP;
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
