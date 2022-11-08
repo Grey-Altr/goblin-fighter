5c7002d7261e91f0566fb4dd9f3e49ffae38a3ba
Grey Altr <grey.sifr@gmail.com>
2022-11-08 12:59:00 -0800

    finished robot clickhandler

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 /* Imports */
-import { renderRobit } from "./render-utils.js";
+import { renderRobit } from './render-utils.js';
 /* Get DOM Elements */
 const playerFPEl = document.querySelector('#player-fp');
 const befriendedNumEl = document.querySelector('#befriend-num');
 const form = document.querySelector('form');
-const 
 /* State */
 let playerFP = 10;
 let befriendedRobitCount = 0;
@@ -32,6 +31,35 @@ form.addEventListener('submit', (e) => {
     currentId++;
     robits.push(newRobit);
 });
+
+// robit clickHandler
+function robitClickHandler(robitData) {
+    if (robitData.fp <= 0) return;
+    if (Math.random() < 0.33) {
+        robitData.fp--;
+        alert("You're really impressing " + robitData.name);
+    } else {
+        alert('Keep chatting. ' + robitData.name + " isn't so sure about you...");
+    }
+
+    if (Math.random() < 0.5) {
+        playerFP--;
+        alert('Umm, just an FYI... ' + robitData.name + " isn't feeling this convo...");
+    } else {
+        alert(robitData.name + ' is unamused, but still listening...');
+    }
+
+    if (robitData.fp === 0) {
+        alert('You and ' + robitData.name + "really hit it off!! You're positively magnetic!");
+        befriendedRobitCount++;
+    }
+
+    if (playerFP === 0) {
+        alert(
+            "Wow... Robits just don't seen to find you that interesting. Maybe just stick to organics from now on?"
+        );
+    }
+}
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
